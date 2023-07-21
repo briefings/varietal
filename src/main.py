6a3eefb28bebd5644ccfc8d74e963045ab7a3fe4
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-21 13:11:29 +0100

    Update src/main.py

---
+++ src/main.py
@@ -0,0 +1,32 @@
+"""
+main.py
+"""
+import logging
+import os
+import sys
+
+
+def main():
+    """
+    Entry point
+
+    :return:
+    """
+
+    logger.info('Varietal')
+
+
+if __name__ == '__main__':
+
+    # Paths
+    root = os.getcwd()
+    sys.path.append(root)
+    sys.path.append(os.path.join(root, 'src'))
+
+    # Logging
+    logging.basicConfig(level=logging.INFO,
+                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    logger = logging.getLogger(__name__)
+
+    main()
