fb8b70ee0b66766719e1d11e97e3ba0e5ec012ca
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-22 23:17:21 +0100

    Update src/modelling/kmeans/metrics.py

--- src/modelling/kmeans/metrics.py
+++ src/modelling/kmeans/metrics.py
@@ -1,7 +1,8 @@
-import pandas as pd
+import logging
+
 import dask_ml.cluster as dc
 import numpy as np
-import logging
+import pandas as pd
 
 
 class Metrics:
@@ -16,7 +17,8 @@ class Metrics:
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
 
-    def exc(self, models: [dc.KMeans]):
+    @staticmethod
+    def exc(models: [dc.KMeans]) -> pd.DataFrame:
         """
         model: dc.KMeans
         for model in models:
@@ -28,15 +30,12 @@ class Metrics:
 
         distortions = {models[i].n_clusters: models[i].inertia_
                        for i in range(len(models))}
-
         frame = pd.DataFrame.from_dict(distortions, orient='index', columns=['distortion'])
         frame.reset_index(drop=False, inplace=True)
         frame.rename(columns={'index': 'n_clusters'}, inplace=True)
-        self.__logger.info(frame)
 
         differences = np.diff(frame['distortion'].to_numpy())
         differences = np.insert(differences.copy(), obj=0, values=np.nan, axis=0)
-        self.__logger.info(differences)
-
         frame.loc[:, 'difference'] = differences
-        self.__logger.info(frame)
+
+        return frame
