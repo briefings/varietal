f466b0ec69dc8fd6757e32821c4be4a209e31517
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-22 16:20:31 +0100

    Update src/data.py

--- src/data.py
+++ src/data.py
@@ -14,11 +14,10 @@ class Data:
         self.__streams = src.functions.streams.Streams()
 
     def offers(self) -> pd.DataFrame:
-
         uri = 'https://raw.githubusercontent.com/miscellane/hub/develop/data/wine/offers.csv'
-        fields={'Offer #': 'offer_id', 'Campaign': 'campaign', 'Varietal': 'varietal',
-                'Minimum Qty (kg)': 'minimum_qty_kg', 'Discount (%)': 'discount_percentage',
-                'Origin': 'origin', 'Past Peak': 'past_peak'}
+        fields = {'Offer #': 'offer_id', 'Campaign': 'campaign', 'Varietal': 'varietal',
+                  'Minimum Qty (kg)': 'minimum_qty_kg', 'Discount (%)': 'discount_percentage',
+                  'Origin': 'origin', 'Past Peak': 'past_peak'}
 
         data = self.__streams.api(uri=uri, header=0)
         data.rename(columns=fields, inplace=True)
