dc3079442ea4f92ed76c07bed63598b1e37d989a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-22 16:41:05 +0100

    Update src/data.py

--- src/data.py
+++ src/data.py
@@ -29,9 +29,10 @@ class Data:
 
     def transactions(self) -> pd.DataFrame:
         uri = 'https://raw.githubusercontent.com/miscellane/hub/develop/data/wine/transactions.csv'
-        fields = {'Offer #': 'offer_id', 'Customer Last Name': 'customer_name'}
+        fields = {'Offer #': 'offer_id', 'Customer Last Name': 'surname'}
 
         data = self.__streams.api(uri=uri, header=0)
         data.rename(columns=fields, inplace=True)
+        data.loc[:, 'surname'] = data['surname'].str.title()
 
         return data
